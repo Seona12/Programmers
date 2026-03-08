49efac6c56006f295c74ed0c18cf58f16ca5705b
Pyun Seon a <139514905+Seona12@users.noreply.github.com>
2026-03-08 23:32:43 +0900

    [Silver IV] Title: 주몽, Time: 172 ms, Memory: 17140 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,42 +1,29 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args)throws IOException{
-        BufferedReader buffredReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(buffredReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
-
-        stringTokenizer = new StringTokenizer(buffredReader.readLine());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
-
-        stringTokenizer = new StringTokenizer(buffredReader.readLine());
-        int A[] = new int[N];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
-
-            A[i] = Integer.parseInt(stringTokenizer.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(A); //배열 오름차순 정렬
-
-        int start_index = 0;
-        int end_index = N-1;
-        int count = 0;
-
-        while(start_index < end_index){
-            int sum = A[start_index] + A[end_index];
-            if(sum == M){
-                count++;
-                start_index++;
-                end_index--;
-
-            }
-            else if(sum < M){
-                start_index++;
-            }else{
-                end_index--;
-            }
-        }
+       Arrays.sort(A);
+       int count = 0;
+       int i = 0; //A[0] -> MIN
+       int j = N-1; //A[N-1] -> MAX
+        
+       while(i < j){ //두 개의 포인터가 겹쳐지는 순간 반복문 종료
+           if(A[i] + A[j] < M) i++;
+           else if(A[i] + A[j] > M)j--;
+           else{
+               count++;
+               i++; j--;
+           }
+       }
         System.out.println(count);
-
     }
 }
