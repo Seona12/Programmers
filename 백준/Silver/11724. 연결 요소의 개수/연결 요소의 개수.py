af590c4badbcc4501e265287748651920b2d72a2
Pyun Seon a <139514905+Seona12@users.noreply.github.com>
2026-03-25 14:50:29 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 668 ms, Memory: 66052 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,28 +1,30 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-def dfs(node):
-    visited[node] = True  # 현재 노드를 방문 처리
-    for neighbor in graph[node]:  # 연결된 모든 노드 탐색
-        if not visited[neighbor]:  # 방문하지 않았다면 DFS 재귀 호출
-            dfs(neighbor)
+N,M = map(int,input().split())
+#인접리스트 a 초기화
+A = [[] for _ in range(N+1)]
+#방문 배열 visited 초기화
+visited = [False] * (N+1)
 
-# 입력 받기
-n, m = map(int, sys.stdin.readline().split())  # 정점 개수, 간선 개수 입력
-graph = [[] for _ in range(n + 1)]  # 그래프 인접 리스트 생성
-visited = [False] * (n + 1)  # 방문 여부 리스트
+for i in range(M):
+    u,v = map(int,input().split())
+    A[u].append(v)
+    A[v].append(u)
 
-# 그래프 구성
-for _ in range(m):
-    u, v = map(int, sys.stdin.readline().split())
-    graph[u].append(v)
-    graph[v].append(u)
+def DFS(v):
+    visited[v] = True
+    for i in A[v]:
+        if not visited[i]:
+            DFS(i)
 
-# 연결 요소 개수 세기
 count = 0
-for i in range(1, n + 1):
-    if not visited[i]:  # 방문하지 않은 노드가 있다면
-        dfs(i)  # DFS 탐색 수행
-        count += 1  # 연결 요소 개수 증가
 
-print(count)  # 결과 출력
+for i in range(1,N+1):
+    if not visited[i]:
+        count += 1
+        DFS(i)
+
+print(count)
+    
